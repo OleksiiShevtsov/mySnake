99c81ad1158292b4b904cb1ab6598fd44f6d7398
OleksiiShevtsov <70405962+OleksiiShevtsov@users.noreply.github.com>
2020-11-11 16:33:35 +0200

    Update Source.cpp

--- Snake_2/Source.cpp
+++ Snake_2/Source.cpp
@@ -4,20 +4,14 @@
 
 using namespace std;
 
-//длина, высота в кубиках
 int X = 30, Y = 20;
-//масштаб в одном кубике 
 int Scale = 25;
 
-//длина, высота формы
 int L = Scale * X;
 int H = Scale * Y;
 
-//dir - направление движение
-//num - размер змейки
 int dir = 4;
 
-//параметры змейки
 struct Snake {
 	public:
 		int x;
@@ -30,7 +24,6 @@ struct Snake {
 };
 vector <Snake> snakeVec;
 
-//параметры фрукта
 struct Fruct
 {
 public:
@@ -49,34 +42,28 @@ public:
 
 Fruct fruct;
 
-//шаг для змейки
 void Tick() {
 	
-	//координаты хвоста змейки
 	int temp_x = snakeVec[snakeVec.size() - 1].x;
 	int temp_y = snakeVec[snakeVec.size() - 1].y;
 
-	//для всех кроме первой
 	if (snakeVec.size() > 1) {
 		for (int i = snakeVec.size(); i > 1; --i) {
 			snakeVec[i - 1].x = snakeVec[i - 2].x;
 			snakeVec[i - 1].y = snakeVec[i - 2].y;
 		}
 	}
-	//для первой 
+	
 	if (dir == 0)snakeVec[0].y += 1;
 	if (dir == 1)snakeVec[0].x -= 1;
 	if (dir == 2)snakeVec[0].x += 1;
 	if (dir == 3)snakeVec[0].y -= 1;
-	//if (dir == 4);
-
-	//провекрка змейка ест фрукт 
+	
 	if ((snakeVec[0].x == fruct.x) && (snakeVec[0].y == fruct.y)) {
 		snakeVec.push_back(Snake(temp_x, temp_y));
 		fruct.NewFruct();
 	}
 
-	//проверка выхода за границы
 	if (snakeVec[0].x > X) {
 		snakeVec[0].x = 0;
 	}
@@ -90,7 +77,6 @@ void Tick() {
 		snakeVec[0].y = Y;
 	}
 
-	//проверка если змейка наступает на саму себя то - 1
 	for (int i = 1; i < snakeVec.size(); i++) {
 		if ((snakeVec[0].x == snakeVec[i].x) && (snakeVec[0].y == snakeVec[i].y)) {
 			snakeVec.pop_back();
@@ -98,7 +84,6 @@ void Tick() {
 	}
 }
 
-//рисует форму
 void DrawFiled() {
 	glColor3f(0.2, 0.2, 0.2);
 	glBegin(GL_LINES);
@@ -114,24 +99,19 @@ void DrawFiled() {
 	glEnd();
 }
 
-//рисует змейку
 void DrawSnake() {
 
-	//цвет змейки
 	glColor3f(0.4, 0.4, 0.4);
 	for (int i = 1; i < snakeVec.size(); i++) {
-		//создание прямоугольника
 		glRectf(snakeVec[i].x*Scale, snakeVec[i].y*Scale, (snakeVec[i].x + 0.96)*Scale, (snakeVec[i].y + 0.96)*Scale);
 	}
 
-	//цвет головы
 	glColor3f(0.8, 0.7, 0.1);
 	glRectf(snakeVec[0].x*Scale, snakeVec[0].y*Scale, (snakeVec[0].x + 0.96)*Scale, (snakeVec[0].y + 0.96)*Scale);
 }
 
-//Движение змейки
 void MyKeyboard(unsigned char key, int a, int b) {
-	//проверка ввода клавиш
+	
 	switch (dir) {
 	case 0:
 		switch (key) {
@@ -163,7 +143,6 @@ void MyKeyboard(unsigned char key, int a, int b) {
 	}
 }
 
-//поле
 void display() {
 
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -175,7 +154,6 @@ void display() {
 	glFlush();
 }
 
-//цыкл программы
 void timer(int = 0) {
 
 	display();
@@ -186,7 +164,6 @@ void timer(int = 0) {
 
 int main(int argc, char **argv) {
 
-	//начальные позиции головы змейки
 	snakeVec.push_back(Snake(X / 2, Y / 2));
 	fruct.NewFruct();
 
@@ -194,16 +171,13 @@ int main(int argc, char **argv) {
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(L, H);
 	glutCreateWindow("Snake");
-	glClearColor(0.1, 0.1, 0.1, 0.0);//закрасить поле
+	glClearColor(0.1, 0.1, 0.1, 0.0);//Г§Г ГЄГ°Г Г±ГЁГІГј ГЇГ®Г«ГҐ
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D(0, L, 0, H);
 
-	//функция цыкл времени
 	glutTimerFunc(20, timer, 0);
-	//функция ввода клавиш(не нужно реализовывать)
 	glutKeyboardFunc(MyKeyboard);
-	//функция дисплей
 	glutDisplayFunc(display);
 	glutMainLoop();
 }
